1e5d0cd9d3c169a9b906890ebd654a13eb252188
orels1 <orel-s1@yandex.ru>
2017-07-03 16:24:41 +0300

    fixed footer link

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -6,7 +6,7 @@ import React from 'react';
 const Footer = props => (
   <div className="footer w-100 d-flex flex-row align-items-center justify-content-between padding-30">
     <a href="https://twitter.com/orels1tips">Created by @orels1tips</a>
-    <a href="https://github.com/orels1/test-weather"><img src="/static/img/gh.png" /></a>
+    <a href="https://github.com/orels1/weather-widgets"><img src="/static/img/gh.png" /></a>
   </div>
 );
 
